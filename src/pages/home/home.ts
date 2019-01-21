12d253099c56b0083116b5276bd457181a207e95
starFaby <star._faby@hotmail.com>
2019-01-21 10:56:24 -0500

    camera

---
+++ src/pages/home/home.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  image: string = null;
+  constructor(private camera: Camera) {
+
+  }
+  getPicture(){
+    let options: CameraOptions = {
+      destinationType: this.camera.DestinationType.DATA_URL,
+      targetWidth: 1000,
+      targetHeight: 1000,
+      quality: 100
+    }
+    this.camera.getPicture( options )
+    .then(imageData => {
+      this.image = `data:image/jpeg;base64,${imageData}`;
+    })
+    .catch(error =>{
+      console.error( error );
+    });
+  }
+
+}
